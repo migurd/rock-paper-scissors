cf936ca2d76f5ad8fecea48b00dcf8083c485c51
migurd <angelquimorita@gmail.com>
2022-12-14 21:44:13 -0700

    Added a botton of restart

--- game.js
+++ game.js
@@ -11,23 +11,11 @@ const userScoreTxt = document.querySelector('.score-user')
 const computerScoreTxt = document.querySelector('.score-computer')
 const resultTxt = document.querySelector('.result')
 
-const disableButtons = () => {
-    buttons.forEach(e => {
-        e.disabled = true
-    });
-}
+const restart = document.querySelector('#restart')
+const restartBtn = document.querySelector('#restart__button')
+const overlay = document.querySelector('#overlay')
 
-const isOver = () => {
-    if(userScore === 5 || computerScore === 5) {
-        // Must show a bottom of restart, thus stopping the game
-        disableButtons()
-        if(userScore === 5)
-            resultTxt.textContent = 'User wins!'
-        else
-            resultTxt.textContent = 'Computer wins!'
-    }
-}
-
+// It starts from here when you click any botton
 rock.addEventListener('click', () => getComputerChoice('R'))
 paper.addEventListener('click', () => getComputerChoice('P'))
 scissors.addEventListener('click', () => getComputerChoice('S'))
@@ -79,5 +67,46 @@ const winner = (userChoice, computerChoice) => {
     }
     userScoreTxt.textContent = `USER: ${userScore}`
     computerScoreTxt.textContent = `COMPUTER: ${computerScore}`
+    // Each time there's a win, lose or tie, it's gonna check if any of the member has 5 points
     isOver()
 }
+
+// UTILITY
+const buttonsState = (state) => {
+    buttons.forEach(e => {
+        e.disabled = state
+    });
+}
+
+const popUp = () => {
+    restart.classList.add('active')
+    overlay.classList.add('active')
+    // It's called like restartGame instead of restartGame()because it runs instantly if you call it as a function, but in the other hand, when you call it as a name, it runs after the event 'click' happens
+    restartBtn.addEventListener('click',  restartGame)
+}
+
+const restartGame = () => {
+    restart.classList.remove('active')
+    overlay.classList.remove('active')
+    buttonsState(false)
+    userScore = 0
+    computerScore = 0
+    infoText.textContent = ""
+    userScoreTxt.textContent = ""
+    computerScoreTxt.textContent = ""
+}
+
+const isOver = () => {
+    if(userScore === 5 || computerScore === 5) {
+        // Must show a bottom of restart, thus stopping the game
+        buttonsState(true)
+        if(userScore === 5)
+            resultTxt.textContent = `USER WINS ${userScore}:${computerScore}!`
+        else
+        resultTxt.textContent = `COMPUTER WINS ${computerScore}:${userScore}!`
+        popUp()
+    }
+}
+
+
+// From someone that came from C, it feels so weird don't defining the type like int variable;
